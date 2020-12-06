9585d0a8b46ea87bcefc87a8ff4f06f804176835
Nariboy <46358843+Nariboy@users.noreply.github.com>
2020-12-06 17:54:14 -0300

    listo

---
+++ client/build/precache-manifest.ac225da6f897fed8542755f6bc4e1e54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f82129306037d5e0abafbea36afe5ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "147a9a72637292699470",
+    "url": "/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "10b83a1ff8261a458dae",
+    "url": "/static/css/main.947f2138.chunk.css"
+  },
+  {
+    "revision": "147a9a72637292699470",
+    "url": "/static/js/2.52c2ceaf.chunk.js"
+  },
+  {
+    "revision": "10b83a1ff8261a458dae",
+    "url": "/static/js/main.1f2a391e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0e4c6b31feadee72442de6770cb9ed67",
+    "url": "/static/media/Logo.0e4c6b31.png"
+  }
+]);
